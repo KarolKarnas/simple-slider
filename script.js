f6a2377d2b4e1e521943f48ddb80450e478db605
KarolKarnas <karol.karnas@gmail.com>
2023-03-21 15:27:39 +0100

    Create basic slide functionality
    create duplicate of the first and the last slide with JS
    animate with transition
    change position with translateX()

--- script.js
+++ script.js
@@ -1,31 +1,65 @@
-let slides = document.querySelectorAll('.slide')
-slides = [...slides]
-let first
-console.log(slides);
+const images = document.querySelectorAll('.slide');
+let imagesDuplicates = [...images];
 
+const size = images[0].clientWidth;
+console.log(size);
 
-function render() {
+let firstDup = images[0].cloneNode(true)
+firstDup.classList.add('first-dup')
+imagesDuplicates.push(firstDup);
 
-    slides.forEach((slide, index) => {
-    slide.style.left = `${index * 1920}px`
-})
+let lastDup = images[images.length - 1].cloneNode(true)
+lastDup.classList.add('last-dup')
+imagesDuplicates.unshift(lastDup);
 
+const slider = document.querySelector('.slider');
+slider.innerHTML = '';
+
+console.log(imagesDuplicates);
+
+function renderOnLoad() {
+    imagesDuplicates.forEach((slide) => {
+        const value = -855;
+        slide.style.transform = `translateX(${value}px)`
+        slider.appendChild(slide);
+    }); 
+}
+renderOnLoad()
+
+let counter = 1;
+let maxCounter = imagesDuplicates.length - 1;
+let minCounter = 0
+
+function renderWithCoutner(counter) {
+    imagesDuplicates.forEach((slide) => {
+        const value = counter * -855;
+        slide.style.transform = `translateX(${value}px)`
+    }); 
 }
 
-window.addEventListener('keydown', (e) => {
-if (e.keyCode === 37) {
-    console.log('leftArrow pressed');
-    first = slides.shift()
-    slides.push(first)
-    console.log(slides);
-    render()
 
-} else if (e.keyCode === 39) {
-    console.log('rightArrow pressed');
-}
+const nextBtn = document.getElementById('next-btn')
+const prevBtn = document.getElementById('prev-btn')
 
-console.log(e.keyCode)
+nextBtn.addEventListener('click', () => {
+if (counter < maxCounter) 
+counter++;
+renderWithCoutner(counter)
+})
+
+prevBtn.addEventListener('click', () => {
+if (counter > minCounter) 
+counter--;
+renderWithCoutner(counter)
 })
 
 
-render()
+
+
+
+
+
+
+
+
+
